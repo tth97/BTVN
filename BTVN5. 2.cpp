617f4291d3c3d06d2c757498ed378c4123a31369
tth97 <92083380+tth97@users.noreply.github.com>
2021-10-23 23:02:54 +0700

    Add files via upload

---
+++ BTVN5. 2.cpp
@@ -0,0 +1,43 @@
+﻿// BTVN 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	double test; // nghiệm
+	cout << " Provide a: ";
+	cin >> a;
+	cout << " Provide b: ";
+	cin >> b;
+
+	if (a == 0) {
+		if (b == 0) {
+			cout << " Infinitely many soltion " << endl; // phương trình vô số nghiêm
+		}
+		else {
+			cout << " Method inexperienced " << endl; // phương trình vô nghiêm
+		}
+	}
+	else {
+		test = (double)-b / a;
+		cout << " The equation that has a solution is: " << test << endl; // phương trình có nghiệm
+	}
+
+	return 0;
+
+	}
+
+		
+		
+	
+
+
+// 2. giai phương trình bậcc 1
+// ax+b=0
+
