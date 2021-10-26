bc7caf7929018f47361f9fb87920fd961fad89c3
tth97 <92083380+tth97@users.noreply.github.com>
2021-10-26 15:22:40 +0700

    Add files via upload

---
+++ BT Số nguyên.cpp
@@ -0,0 +1,25 @@
+﻿// BT Số nguyên.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	int k;
+	int b;
+	cout << " Enter an integer k: ";
+	cin >> k;
+	for (int i = 1; i <= k; i++);
+	cin.ignore(1000, '\n');
+	
+		b = k + 1;
+		cout << " Print the sum of all integer: " << b<< '\n';
+	
+	return 0;
+
+}
+
+//5. Nhập số nguyên K và in ra tổng các số nguyên từ 1 đến k.
+
